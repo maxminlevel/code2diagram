d827285c13b32c786cc8ab9a0b36051677851fcd
maxminlevel <toanvo@peahoki.net>
2022-06-30 21:30:53 +0700

    Temp

--- src/io/reader.py
+++ src/io/reader.py
@@ -3,3 +3,28 @@
 # - Input: file name
 # - Output: instance of FileContent
 # - Use Strategy design pattern to read the file (based on its extension).
+
+from ds.fileContent.fileContent import FileContent
+from ds.object.flexible import FlexibleObject
+
+
+class Reader(FlexibleObject):
+    def __init__(self, file_path):
+        super().__init__()
+        self['input'] = self.absolute_path(file_path)
+        self['input_type'] = self.get_extension(file_path)
+        self['output'] = FileContent()
+
+    def absolute_path(self, file):
+        return ""
+
+    def get_extension(self, file):
+        return "json"
+
+    def stragety(self):
+        pass
+        
+        
+        
+
+    
