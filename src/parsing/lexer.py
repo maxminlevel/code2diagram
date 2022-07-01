18169637f6eb7a37a96ec8d9f4f0af2435d1ae68
pbcuong781 <pbcuong7801@gmail.com>
2022-07-01 14:53:29 +0700

    Lexer : Analyze fileContent to list of tokens

--- src/parsing/lexer.py
+++ src/parsing/lexer.py
@@ -4,71 +4,29 @@
 # - Input: Code content (FileContent)
 # - Output: List of tokens 
 
-from ds.token.token import *
-import json
-from functools import singledispatch
+from parsing.lexerMethod import LexerMethod
 
-obj = '{"attribute": {"name": "Simple","type": "Usecase"},"data": [{"name": "Man","uniq": "1","type": "actor","group": "","attribute": {}},{"name": "do some thing","uniq": "2","type": "usecase","group": "","attribute": {}},{"source": 1,"target": 2, "type":"arrow", "attribute": {}}]}'
-
-class Content(object): pass
-class JsonContent(object): pass
-class CsvContent(Content): pass
-class XmlContent(Content): pass
-
+obj = '{"attribute": {"name": "Simple", "type": "Usecase"}, "data": [{"name": "Man", "uniq": "1", "type": "actor", "group": "", "attribute": {}}, {"name": "do some thing", "uniq": "2", "type": "action", "group": "", "attribute": {}}, {"source": 1, "target": 2, "type": "arrow", "attribute": {}}]}'
 
 class Lexer(object):
-    
     def __init__(self):
-        self.contentToToken = singledispatch(self.contentToToken)
-        self.contentToToken.register(str, self._contentToToken_Json)
-        self.contentToToken.register(CsvContent, self._contentToToken_Csv)
-        self.contentToToken.register(XmlContent, self._contentToToken_Xml)
+        self.type = "Lexer"
+        self.lexerMethod = None
         
-    def contentToToken(self, s):
-        raise TypeError("This type isn't supported: {}".format(type(s)))
+    def analyze(self, fileContent):
+        self.setLexerMethod(self.select_type_lexer_method("json"))
+        return self.lexerMethod.analyze(fileContent)
 
-    def _contentToToken_Json(self, obj):
-        contentJson = json.loads(obj)
-        listTokens = []
-        for item in contentJson["data"]:
-            token = None
-            if item['type'] == "actor":
-                groupId = item['group']
-                if groupId is None:
-                    token = ActorToken(item["type"], item["name"])
-                else:
-                    token = ActorToken(item["type"], item["name"], groupId)
-            elif item["type"] == "arrow":
-                token = ArrowToken(item["type"], item["source"], item["target"], item['attribute'])
-            elif item["type"] == "usecase":
-                token = UsecaseToken(item["type"], item["name"], item["group"])
-            elif item["type"] == "class":
-                token = ClassToken(item["type"], item["name"], item["group"], item["attribute"])
-            
-            listTokens.append(token)
-        
-        return listTokens
+    def setLexerMethod(self, method: LexerMethod):
+        self.lexerMethod = method
 
-    def _contentToToken_Csv(self, obj):
-        print("CSV" +  str(obj))
-
-    def _contentToToken_Xml(self, obj):
-        print("XML" + str(obj))
-
-
+    def select_type_lexer_method(self, typeFileContent: str):
+        lexerMethod = LexerMethod.instance[typeFileContent]
+        if not lexerMethod:
+            raise TypeError("This type isn't supported: {}".format(type(typeFileContent)))
+        return lexerMethod
 
 lexer = Lexer()
-csv = CsvContent()
-xml = XmlContent()
-content = Content()
-content = csv
-listTokens = lexer.contentToToken(obj)
-
-print(listTokens)
-print("\n")
-for token in listTokens:
-    print(token)
-
-lexer.contentToToken(content)
-
+li = lexer.analyze(obj)
+print(li)
 
---
+++ src/parsing/lexerMethod.py
@@ -0,0 +1,70 @@
+from abc import abstractmethod
+from ds.object.flexible import FlexibleObject
+import json
+from ds.token.token import*
+
+class LexerMethod(FlexibleObject):
+    instance = dict()
+    def __init__(self, type):
+        super().__init__()
+        self.type = "Analyze"
+        self.file_type = type
+        LexerMethod.instance[type] = self
+
+    @abstractmethod
+    def analyze(self, fileContent) -> list:
+        pass
+
+class JSON_input(LexerMethod): 
+    def __init__(self):
+        super().__init__('json')
+
+    def analyze(self, fileContent):
+        contentJson = json.loads(fileContent)
+        listTokens = []
+        for item in contentJson["data"]:
+            token = None
+            if item['type'] == "actor":
+                groupId = item['group']
+                if groupId is None:
+                    token = ActorToken(item["type"], item["name"])
+                else:
+                    token = ActorToken(item["type"], item["name"], groupId)
+            elif item["type"] == "arrow":
+                token = ArrowToken(item["type"], item["source"], item["target"], item['attribute'])
+            elif item["type"] == "usecase":
+                token = UsecaseToken(item["type"], item["name"], item["group"])
+            elif item["type"] == "class":
+                token = ClassToken(item["type"], item["name"], item["group"], item["attribute"])
+            
+            listTokens.append(token)
+        
+        return listTokens
+
+class XML_input(LexerMethod): 
+    def __init__(self):
+        super().__init__('xml')
+
+    def analyze(self, fileContent):
+        pass
+
+class TXT_input(LexerMethod): 
+    def __init__(self):
+        super().__init__('txt')
+
+    def analyze(self, fileContent):
+        with open(fileContent, 'r') as f:
+            pass
+
+class CSV_input(LexerMethod): 
+    def __init__(self):
+        super().__init__('csv')
+
+    def analyze(self, fileContent):
+        with open(fileContent, 'r') as f:
+            pass
+
+JSON_input()
+XML_input()
+TXT_input()
+CSV_input()
