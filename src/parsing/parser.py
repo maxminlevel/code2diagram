d4ac4b753b2eab9d49653f319893190a69a58d05
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-07-02 00:52:37 +0700

    add parser for csv format case

--- src/parsing/parser.py
+++ src/parsing/parser.py
@@ -3,3 +3,18 @@
 #  - Output: A graph:
 #   - Node: Actor, usecase, class
 #   - Edges: Different kinds of arrow
+from ds.umlGraph import UMLGraph, Node, Edge
+from parsing.umlGraphConfig import ACTOR_TYPE, USECASE_TYPE, ARROW_TYPE
+class Parser:
+    tokenToGraphObjMapping = {
+        ACTOR_TYPE: Node,
+        USECASE_TYPE: Node,
+        ARROW_TYPE: Edge,
+    }
+    @staticmethod
+    def parse(tokens):
+        graph = UMLGraph()
+        for token in tokens:
+            graphObj = Parser.tokenToGraphObjMapping[token.type](token.value, token.type, token.attributes)
+            graph.add(graphObj)
+        return graph
