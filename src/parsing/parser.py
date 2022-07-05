e9b467e630ff9be5d61be5b8fc372df4748ba97e
binh-nguyen49-jh <binh.nguyen@journeyh.io>
2022-07-05 23:10:28 +0700

    add new supporting token type

--- src/parsing/parser.py
+++ src/parsing/parser.py
@@ -4,19 +4,21 @@
 #   - Node: Actor, usecase, class
 #   - Edges: Different kinds of arrow
 from ds.umlGraph.umlEdge import EdgeFactory
-from ds.umlGraph.umlGraph import UMLGraph, Edge
+from ds.umlGraph.umlGraph import UMLGraph
 from ds.umlGraph.umlNode import NodeFactory
-from parsing.umlGraphConfig import ACTOR_TYPE, EDGE_TYPE, NODE_TYPE, USECASE_TYPE, ARROW_TYPE
+from ds.umlGraph.umlGraphConfig import ACTOR_TYPE, USECASE_TYPE, ARROW_TYPE, CLASS_TYPE
 class Parser:
     tokenToFactory = {
         ACTOR_TYPE: NodeFactory,
         USECASE_TYPE: NodeFactory,
         ARROW_TYPE: EdgeFactory,
+        CLASS_TYPE: NodeFactory
     }
     @staticmethod
     def parse(tokens):
         graph = UMLGraph()
         for token in tokens:
             factory = Parser.tokenToFactory[token.type]
-            graph.add(factory)
+            graphObj = factory.create(token)
+            graph.add(graphObj)
         return graph
