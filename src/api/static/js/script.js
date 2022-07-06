2fa8b3ad3c81e440df03566c2b8dea44fd6e052b
maxminlevel <toanvo@peahoki.net>
2022-07-06 18:20:17 +0700

    Prototype for script

---
+++ src/api/static/js/script.js
@@ -0,0 +1,13 @@
+console.log("Console of Code to Diagram");
+
+let btUpload = document.getElementById("upload");
+console.log(btUpload);
+btUpload.onClick = () => {};
+
+let btConvert = document.getElementById("convert");
+console.log(btConvert);
+btConvert.onClick = () => {};
+
+let btDownload = document.getElementById("download");
+console.log(btDownload);
+btUpload.onClick = () => {};
