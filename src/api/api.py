f667bae2f1e6044bc2c9b30e0b68ae35b7d1a2b5
pbcuong781 <pbcuong7801@gmail.com>
2022-07-06 19:37:26 +0700

    add download image

--- src/api/api.py
+++ src/api/api.py
@@ -1,10 +1,27 @@
 from distutils.log import debug
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
   return render_template('index.html')
+
+@app.route('/download_image')
+def download_image():
+	path = "./static/images/usecase.png"
+	return send_file(path, as_attachment=True)
+
+def convert():
+  #do something
+  print("convert")
+  
+
+def upload_file():
+  #do something
+  print("upload")
+  
+
+  
 
 if __name__ == "__main__":
   app.run()
