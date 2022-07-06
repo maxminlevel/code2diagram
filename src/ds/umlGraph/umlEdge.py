0fbf12ae3134fbcb8aa629d198549e71909189bc
huubinh49 <binh.nguyen@journeyh.io>
2022-07-06 20:18:03 +0700

    define render args based on typeRel

--- src/ds/umlGraph/umlEdge.py
+++ src/ds/umlGraph/umlEdge.py
@@ -3,10 +3,11 @@ from uuid import uuid4
 
 from ds.token.token import ArrowToken, TypeRelation
 class Edge:
-    def __init__(self, type, srcId, dstId):
+    def __init__(self, type, srcId, dstId, attrs):
         self.srcId = srcId
         self.dstId = dstId
         self.type = type
+        self.attrs = attrs
         self.id = str(uuid4())
     def __str__(self):
         return self.srcId + " " + self.dstId
@@ -14,12 +15,22 @@ class Edge:
         return self.srcId, self.dstId
 
 class ArrowEdge(Edge):
-    def __init__(self, type, srcId, dstId):
-        super().__init__(type, srcId, dstId)
+    def __init__(self, type, srcId, dstId, attrs):
+        super().__init__(type, srcId, dstId, attrs)
     def __str__(self):
         return self.srcId + " " + self.type + " " + self.dstId
     def toDotArgs(self):
-        return self.srcId, self.dstId
+        relationType = self.attrs['relation']
+        if relationType == TypeRelation.Extend or relationType == TypeRelation.Include:
+            return self.srcId, self.dstId, { 'style': 'dashed' }
+        elif relationType == TypeRelation.OneToOne:
+            return self.srcId, self.dstId, { 'style': 'solid' }
+        elif relationType == TypeRelation.OneToMany:
+            return self.srcId, self.dstId, { 'style': 'dashed' }
+        elif relationType == TypeRelation.ManyToMany:
+            return self.srcId, self.dstId, { 'style': 'solid' }
+        else:
+            return self.srcId, self.dstId, { }
 
 class EdgeFactory:
     @singledispatch
